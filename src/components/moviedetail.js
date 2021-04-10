506d40bc886b4b0a01958b912b528abc3261a875
nguyenviv <72362744+nguyenviv@users.noreply.github.com>
2021-04-10 11:36:45 -0600

    Update moviedetail

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -64,7 +64,7 @@ class MovieDetail extends Component {
                                     <b>{actor.actorName}</b> {actor.characterName}
                                 </p>)}
                         </ListGroupItem>
-                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.rating}</h4></ListGroupItem>
+                        <ListGroupItem><h4><BsStarFill/> {this.props.selectedMovie.averaged_rating}</h4></ListGroupItem>
                     </ListGroup>
                     <Card.Body>
                         {this.props.selectedMovie.reviews.map((review, i) =>
